140b66ab7d5d8ab58e6a4195027626ae92e821a9
rushmeshrawak <rashmi.shrawak@globallogic.com>
2025-02-28 11:35:13 +0000

    Copilor second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 20,
+        skills: ['JavaScript', 'React', 'Node', 'MongoDB'],
+        isMarried: false
+    };
+
+    return member;
+}
